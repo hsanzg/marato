49b2f348b3da7f5e047eac59fc49c6fd09ca1243
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2023-12-16 20:14:01 +0100

    initial integration

--- public/input.js
+++ public/input.js
@@ -3,25 +3,29 @@ activeTabIndicator.className = 'h-0.5 absolute inset-x-0 bottom-0 bg-green-300';
 
 const padTabButton = document.getElementById('tab-pad');
 const tamponTabButton = document.getElementById('tab-tampon');
+const pad = document.getElementById('pad');
+const tampon = document.getElementById('tampon');
 let activeTab = null;
 
-function toggleTabButton(name, old, next) {
+function toggleTabButton(name, oldButton, nextButton, old, next) {
   if (activeTab === name) return;
-  old.classList.remove('text-black');
-  if (old.lastElementChild) {
-    old.removeChild(old.lastElementChild);
+  oldButton.classList.remove('text-black');
+  old.classList.add('hidden');
+  if (oldButton.lastElementChild) {
+    oldButton.removeChild(oldButton.lastElementChild);
   }
-  next.classList.add('text-black');
-  next.appendChild(activeTabIndicator);
+  nextButton.classList.add('text-black');
+  nextButton.appendChild(activeTabIndicator);
+  next.classList.remove('hidden');
   activeTab = name;
 }
 
 function selectPad() {
-  toggleTabButton('pad', tamponTabButton, padTabButton);
+  toggleTabButton('pad', tamponTabButton, padTabButton, tampon, pad);
 }
 
 function selectTampon() {
-  toggleTabButton('tampon', padTabButton, tamponTabButton);
+  toggleTabButton('tampon', padTabButton, tamponTabButton, pad, tampon);
 }
 
 selectPad();
