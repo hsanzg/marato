c997aaab1ba129db0395714cab6aff0a6ab7420d
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2023-12-17 11:50:02 +0100

    changes

--- public/history.js
+++ public/history.js
@@ -1,5 +1,22 @@
 const history = document.getElementById('history');
 
+function createCircle(entry) {
+  /*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+        <circle cx="12" cy="12" r="12" />
+      </svg>*/
+  const svg = document.createElement('svg');
+  svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  svg.setAttribute('viewBox', '0 0 24 24');
+  svg.setAttribute('fill', 'currentColor');
+  svg.className = `w-4 h-4 text-red-400`;
+  const circle = document.createElement('circle');
+  circle.setAttribute('cx', 12);
+  circle.setAttribute('cy', 12);
+  circle.setAttribute('r', 12);
+  svg.appendChild(circle);
+  return svg;
+}
+
 function createCycleEntry(cycle) {
   const cycleElem = document.createElement('li');
   cycleElem.className = 'flex justify-between gap-x-6 py-5';
@@ -8,8 +25,11 @@ function createCycleEntry(cycle) {
   cycleElem.appendChild(entries);
   for (const entry of cycle.entries) {
     const {level, kind} = entry;
-    const elem = document.createElement('div');
-    elem.innerText = level + ' ' + kind;
+    //const elem = document.createElement('div');
+    const elem = createCircle(entry);
+    elem.classList.add('rounded-full');
+    elem.classList.add('bg-red-400');
+    //elem.innerText = level + ' ' + kind;
     entries.appendChild(elem);
   }
 
